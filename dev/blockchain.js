b16612aa846c4b67ef38cf6fdd14821fc058f949
mainegomedia <mainegomedia@gmail.com>
2022-01-03 19:34:46 -0500

    blockchain and test

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,10 @@
+const sha256 = require("sha256");
+
 function Blockchain() {
     this.chain = [];
     this.newPendingTransactions = [];
+
+    this.createNewBlock(100, '0', '0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
@@ -9,6 +13,7 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
         timestamp: Date.now(),
         transactions: this.newPendingTransactions,
         nonce:nonce,
+        hash: hash,
         previousBlockHash: previousBlockHash
     }
 
@@ -32,5 +37,22 @@ const newTransaction = {
 
     return this.getLastBlock()['index'] + 1
 }
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
+     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+     const hash = sha256(dataAsString);
+     return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    let nonce = 0;
+    let hash =  this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0,4) !== "0000") {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+
+    return nonce;
+} 
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,22 +1,23 @@
 const Blockchain = require("./blockchain");
-
 const jermcoin = new Blockchain();
 
-jermcoin.createNewBlock(9461113, "2403UJEIFJKFDFLK", "DKFJALF849U8JEJAF")
-jermcoin.createNewTransaction(100, "BOB8490TEIRGIJGKALGJ","I35980EFSODFAJLKDKF")
-jermcoin.createNewBlock(9448953, "2403UJEIFJghffdfdh", "DKFJALF84TYRUJGHHG")
-
-jermcoin.createNewTransaction(50, "BOB8490TEIRGIJGKALGJ","I35980EFSODFAJLKDKF")
-jermcoin.createNewTransaction(1135, "BOB8490TEIRGIJGKALGJ","I35980EFSODFAJLKDKF")
-jermcoin.createNewTransaction(26, "BOB8490TEIRGIJGKALGJ","I35980EFSODFAJLKDKF")
-
-jermcoin.createNewBlock(9448953, "2403UJEIFJghffdfdh", "DKFJALF84TYRUJGHHG")
-
-
-
-
-
-
-
+const previousBlockHash = '1EIRIUE23943802J3R399384';
+const currentBlockData = [
+    {
+        amount: 101,
+        sender: '234897584795498EWJEWOQAJEW',
+        recipient: 'EUORIEQWPUR38740324294893'
+    },
+    {
+        amount: 1150,
+        sender: '234897584795498EWJEWOQAJEW',
+        recipient: 'EUORIEQWPUR38740324294893'
+    },
+    {
+        amount: 1548,
+        sender: '234897584795498EWJEWOQAJEW',
+        recipient: 'EUORIEQWPUR38740324294893'
+    },
+]
 
 console.log(jermcoin);
